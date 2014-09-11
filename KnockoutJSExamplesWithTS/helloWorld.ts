c35193ecbea38a11c36dedbe5e44bc125be164e5
朱永光 <zyg@ike-global.com>
2014-09-11 16:30:15 +0800

    Introductory examples (but have ts compile error)

--- KnockoutJSExamplesWithTS/helloWorld.ts
+++ KnockoutJSExamplesWithTS/helloWorld.ts
@@ -1,10 +1,11 @@
-﻿var ViewModel = function (first:string, last:string) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
+﻿var ViewModel
+    = function (first: string, last: string) {
+        this.firstName = ko.observable(first);
+        this.lastName = ko.observable(last);
 
-    this.fullName = ko.pureComputed(function () {
-        return this.firstName() + " " + this.lastName();
-    }, this);
-};
+        this.fullName = ko.pureComputed(function () {
+            return this.firstName() + " " + this.lastName();
+        }, this);
+    };
 
 ko.applyBindings(new ViewModel("TypeScript", "KnockoutJS"));
